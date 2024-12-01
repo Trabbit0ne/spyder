d1b2167316c733479c7496c2253b84e4c1fdac93
Trabbit0ne <trabbit0neyt@gmail.com>
2024-12-01 15:52:29 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,10 +5,10 @@ clear
 
 # Variables
 username=""
-target=""
+choice=""
 
 # Set color code shortcuts
-red="\e[31m"    # Red text code
+red="\e[1;31m"    # Red text code
 green="\e[32m"    # Green text code
 yellow="\e[33m"   # Yellow text code
 blue="\e[34m"     # Blue text code
@@ -28,8 +28,24 @@ check_platform() {
   local url
 
   case $platform in
-    "x" | "twitter") url="https://x.com/$username";;
-    "instagram") url="https://www.instagram.com/$username";;
+    "twitter")
+      results=$(python3 checkers/xcheck.py "$username")
+      if [ "$result" == "True" ]; then
+        echo -e "[\e[32m+\e[0m] Username '\e[32m$username\e[0m' ~> \e[32m$platform\e[0m [\e[32m√\e[0m]"
+      else
+        echo -e "[\e[31m-\e[0m] Username '\e[31m$username\e[0m' ~> \e[31m$platform\e[0m [\e[31mx\e[0m]"
+      fi
+      return
+      ;;
+    "instagram")
+      result=$(python3 checkers/instacheck.py "$username")
+      if [ "$result" == "True" ]; then
+        echo -e "[\e[32m+\e[0m] Username '\e[32m$username\e[0m' ~> \e[32m$platform\e[0m [\e[32m√\e[0m]"
+      else
+        echo -e "[\e[31m-\e[0m] Username '\e[31m$username\e[0m' ~> \e[31m$platform\e[0m [\e[31mx\e[0m]"
+      fi
+      return
+      ;;
     "facebook") url="https://m.facebook.com/$username";;
     "github") url="https://github.com/$username";;
     "linkedin") url="https://www.linkedin.com/in/$username";;
@@ -42,14 +58,12 @@ check_platform() {
       return;;
   esac
 
-  response=$(curl -s -L -o /dev/null -w "%{http_code}" -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36" "$url")
+  response=$(curl -s -o /dev/null -L -w "%{http_code}" "$url")
 
   if [ "$response" -eq 200 ]; then
     echo -e "[\e[32m+\e[0m] Username '\e[32m$username\e[0m' ~> \e[32m$platform\e[0m [\e[32m√\e[0m]"
-  elif [ "$response" -eq 404 ]; then
-    echo -e "[${red}-\e[0m] Username '${red}$username\e[0m' ~> ${red}$platform\e[0m [${red}x\e[0m]"
-  elif [ "$response" -eq 301 ] || [ "$response" -eq 302 ]; then
-    echo -e "[\e[33m?\e[0m] Redirected for \e[33m$username\e[0m on \e[33m$platform\e[0m [\e[33m!\e[0m]"
+  elif [ "$response" -eq 404 ] || [ "$response" -eq 301 ]; then
+    echo -e "[\e[31m-\e[0m] Username '\e[31m$username\e[0m' ~> \e[31m$platform\e[0m [\e[31mx\e[0m] $response"
   else
     echo -e "[\e[35m!\e[0m] Unable to determine validity on \e[35m$platform\e[0m [\e[35m!\e[0m]"
   fi
@@ -68,28 +82,25 @@ check_nsfw_platform() {
       return;;
   esac
 
-  response=$(curl -s -L -o /dev/null -w "%{http_code}" -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36" "$url")
+  response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
 
   if [ "$response" -eq 200 ]; then
     echo -e "[\e[32m+\e[0m] Username '\e[32m$username\e[0m' ~> \e[32m$platform_nsfw\e[0m [\e[32m√\e[0m]"
   elif [ "$response" -eq 404 ]; then
-    echo -e "[${red}-\e[0m] Username '${red}$username\e[0m' ~> ${red}$platform_nsfw\e[0m [${red}x\e[0m]"
+    echo -e "[\e[31m-\e[0m] Username '\e[31m$username\e[0m' ~> \e[31m$platform_nsfw\e[0m [\e[31mx\e[0m]"
   else
-    echo -e "[\e[35m!\e[0m] Unable to determine validity on \e[35m$platform_nsfw\e[0m [\e[35m!\e[0m]"
+    echo -e "[\e[35m!\e[0m] Unable to determine validity on \e[35m$platform_nsfw\e[0m ~> $response [\e[35m!\e[0m]"
   fi
 }
 
 # Get Options/Arguments Used
-if [[ -z "$target" ]]; then
-    target="ALL"
-fi
-while getopts ":u:t:" opt; do
+while getopts ":u:s:" opt; do
   case $opt in
     u)
       username=$OPTARG
       ;;
-    t)
-      target=$OPTARG
+    s)
+      choice=$OPTARG
       ;;
     *)
       echo "Invalid option: -$OPTARG" >&2
@@ -99,7 +110,8 @@ while getopts ":u:t:" opt; do
 done
 
 # Banner
-echo -e "\e[34m                                  =**=              :***.                                 "
+echo
+echo -e "${blue}                                  =**=              :***.                                 "
 echo "                                =@@@-                .#@@*.                               "
 echo "                              =%@@+                    :#@@+.                             "
 echo "               -            =@@@*.                       =@@@*.           ..              "
@@ -109,70 +121,62 @@ echo "               -@*        +@@*      -#@@@@@@+ @@@@@@%+      -@@%        :@
 echo "                %@-       #@@-     :#@@@@@@@+ @@@@@@@%+      @@@:      .@@:              "
 echo "                =@@.     .@@@.   .#*=:::=*%@+ @%#+-::-+#+    #@@+      #@#                "
 echo "                 @@#     =@@%    %@@@@@@#+: . . =*%@@@@@@:   =@@@     =@@-                "
-echo -e "                 =@@=    %@@+    +@@@@@@%-${red}.== *-.\e[34m*@@@@@@#:   :@@@:   .@@%                 "
-echo -e "                  %@@:   %@@*   :=.-#@*::${red}*@@+ @@%=\e[34m.=%@+.-+   :@@@-   %@@-                 "
-echo -e "                  =@@@=  .%@@*   @@*  .${red}#@@@@- #@@@@=\e[34m  -%@+  -@@@=  .#@@%                  "
-echo -e "                   =@@@#. .%@@#. =+.-*:.${red}+%=.. ::*%-.=+\e[34m.-*  +@@@-  +@@@*.                  "
-echo -e "                    .#@@@+  %@@%. .%@@@#  ${red}=%+ @#: \e[34m-@@@@+  +@@@- -%@@@-                    "
-echo -e "                      -@@@%: #@@@  .*%=.${red}=@@@+ @@@#\e[34m::*%- :*@@@- *@@@+                      "
-echo -e "        .:=+=-::.       **::+:+@@@@*. ${red}:%@@@@+ @@@@@*\e[34m  -%@@@#-+-.=#:       .:-=+=-.        "
-echo -e "           .-+%@@%#+-.    .-+*+:-*@@@*:.${red}+@@@+ @@@#-\e[34m.=%@@%=:=*+=:     :=*%@@@*=:           "
-echo -e "               .=*@@@@@#+--::..    -*%@%=.${red}=%+ @*:\e[34m.*@@#=.   ...::-=*%@@@@#+:               "
+echo -e "                 =@@=    %@@+    +@@@@@@%-\e[31m.== *-.${blue}*@@@@@@#:   :@@@:   .@@%                 "
+echo -e "                  %@@:   %@@*   :=.-#@*::\e[31m*@@+ @@%=${blue}.=%@+.-+   :@@@-   %@@-                 "
+echo -e "                  =@@@=  .%@@*   @@*  .\e[31m#@@@@- #@@@@=${blue}  -%@+  -@@@=  .#@@%                  "
+echo -e "                   =@@@#. .%@@#. =+.-*:.\e[31m+%=.. ::*%-.=+${blue}.-*  +@@@-  +@@@*.                  "
+echo -e "                    .#@@@+  %@@%. .%@@@#  \e[31m=%+ @#: ${blue}-@@@@+  +@@@- -%@@@-                    "
+echo -e "                      -@@@%: #@@@  .*%=.\e[31m=@@@+ @@@#${blue}::*%- :*@@@- *@@@+                      "
+echo -e "        .:=+=-::.       **::+:+@@@@*. \e[31m:%@@@@+ @@@@@*${blue}  -%@@@#-+-.=#:       .:-=+=-.        "
+echo -e "           .-+%@@%#+-.    .-+*+:-*@@@*:.\e[31m+@@@+ @@@#-${blue}.=%@@%=:=*+=:     :=*%@@@*=:           "
+echo -e "               .=*@@@@@#+--::..    -*%@%=.\e[31m=%+ @*:${blue}.*@@#=.   ...::-=*%@@@@#+:               "
 echo "                   .=*%@@@@@@@@@@@@%+-::--  . . .-:::=*@@@@@@@@@@@@@#+:                   "
 echo "                       .=++***##%%@@@@@@#- .: - .+%@@@@@@%%##**++=:                       "
 echo -e "                           ..:-=+*#%%#=\e[.-*%@+ @@#=::+%%#*+=--:.                           "
-echo -e "                     +*#%%@@@@@@@@@*.\e[38;5;92m-#@@@@@+ @@@@@%+\e[34m -#@@@@@@@@@%#*+:                    "
-echo -e "                    +@@@%%#**+==-: \e[38;5;92m:#@@@#@@@+ @@@@%@@@+\e[34m .--=++*##%@@@%                    "
-echo -e "                   .@@@-         \e[38;5;92m:#@@@+.+@@@+ @@@#.-%@@@+\e[34m          @@@=                   "
-echo -e "                   #@@#        \e[38;5;92m*@@@*. *=.-*= #=.*- =%@@%=\e[34m          =@@@.                  "
-echo -e "                  -@@@:      \e[38;5;92m.*@@@*:  =@@@%+. -*@@@@.  =%@@%\e[34m        #@@#                  "
-echo -e "                  %@@+       \e[38;5;92m@@@%:    -*@@@@+ @@@@%+.    +@@@=\e[34m      .@@@-                 "
-echo -e "                 *@@%        \e[38;5;92m#@@+    .+=.-*@+ @#+::+-    .@@@:\e[34m       +@@@.                "
-echo -e "                 *@@%.       \e[38;5;92m+@@#     .%@#= . ..+@@+     -@@@\e[34m        *@@@:                "
-echo -e "                  +@@%.      \e[38;5;92m-@@@      .@%:     +@+      +@@*\e[34m       +@@#.                 "
-echo -e "                   -@@%      \e[38;5;92m.@@@:      .        :       %@@=\e[34m      =@@*                   "
-echo -e "                    :@@*      \e[38;5;92m%@@=                       @@@.\e[34m     -@@+\e[34m                    "
-echo -e "                     .%@=     \e[38;5;92m+@@#                      :@@%\e[34m     .@@-                     "
-echo -e "                       *@-    \e[38;5;92m-@@@.                     *@@*\e[34m     %%:                      "
-echo -e "                        +@:    \e[38;5;92m*@@@.                   +@@%:\e[34m    #%.                       "
-echo -e "                         -%     \e[38;5;92m.*@@:                 *@%-\e[34m     **                         "
-echo -e "                          .+      \e[38;5;92m.+%.               *#-\e[34m      --                          "
-echo -e "                            .       \e[38;5;92m.=.             =:\e[34m        .                           "
+echo -e "                     +*#%%@@@@@@@@@*.\e[38;5;92m-#@@@@@+ @@@@@%+${blue} -#@@@@@@@@@%#*+:                    "
+echo -e "                    +@@@%%#**+==-: \e[38;5;92m:#@@@#@@@+ @@@@%@@@+${blue} .--=++*##%@@@%                    "
+echo -e "                   .@@@-         \e[38;5;92m:#@@@+.+@@@+ @@@#.-%@@@+${blue}          @@@=                   "
+echo -e "                   #@@#        \e[38;5;92m*@@@*. *=.-*= #=.*- =%@@%=${blue}          =@@@.                  "
+echo -e "                  -@@@:      \e[38;5;92m.*@@@*:  =@@@%+. -*@@@@.  =%@@%${blue}        #@@#                  "
+echo -e "                  %@@+       \e[38;5;92m@@@%:    -*@@@@+ @@@@%+.    +@@@=${blue}      .@@@-                 "
+echo -e "                 *@@%        \e[38;5;92m#@@+    .+=.-*@+ @#+::+-    .@@@:${blue}       +@@@.                "
+echo -e "                 *@@%.       \e[38;5;92m+@@#     .%@#= . ..+@@+     -@@@${blue}        *@@@:                "
+echo -e "                  +@@%.      \e[38;5;92m-@@@      .@%:     +@+      +@@*${blue}       +@@#.                 "
+echo -e "                   -@@%      \e[38;5;92m.@@@:      .        :       %@@=${blue}      =@@*                   "
+echo -e "                    :@@*      \e[38;5;92m%@@=                       @@@.${blue}     -@@+${blue}                    "
+echo -e "                     .%@=     \e[38;5;92m+@@#                      :@@%${blue}     .@@-                     "
+echo -e "                       *@-    \e[38;5;92m-@@@.                     *@@*${blue}     %%:                      "
+echo -e "                        +@:    \e[38;5;92m*@@@.                   +@@%:${blue}    #%.                       "
+echo -e "                         -%     \e[38;5;92m.*@@:                 *@%-${blue}     **                         "
+echo -e "                          .+      \e[38;5;92m.+%.               *#-${blue}      --                          "
+echo -e "                            .       \e[38;5;92m.=.             =:${blue}        .                           "
 echo -e "                                                                                          "
 echo
-echo -e "                                   \e[33mDEVELOPPED BY \e[32mTRABBIT\e[0m                                 "
+echo -e "                                   ${yellow}DEVELOPPED BY ${green}TRABBIT${clean}                                 "
+
 echo
-echo -e "{+}======================================================={+}"
-echo -e "    Username: ${yellow}$username${clean}"
-echo -e "    Target(s): ${yellow}$target${clean}"
-echo -e "{+}======================================================={+}"
+echo -e "${yellow}[+]${clean} ${green}Author: ${clean} ${blue}@Trabbit0ne ${clean}"
+echo -e "${yellow}[+]${clean} ${green}Github: ${clean} ${blue}github.com/Trabbit0ne ${clean}"
 echo
 
-# Ensure username and target are provided
-if [ -z "$username" ] || [ -z "$target" ]; then
-  echo -e "${red}[-] Username and service target are required. Please provide both.\e[0m"
+if [ -z "$username" ] || [ -z "$choice" ]; then
+  echo "Usage: $0 -u <username> -s <social|nsfw>"
   exit 1
 fi
 
-# Check the chosen services
-case $target in
-  "all" | "ALL")
-    platforms=("twitter" "instagram" "facebook" "github" "linkedin" "tiktok" "snapchat" "reddit" "youtube")
-    for platform in "${platforms[@]}"; do
-      check_platform "$platform"
-    done
-    ;;
-  "x" | "twitter" | "instagram" | "facebook" | "github" | "linkedin" | "tiktok" | "snapchat" | "reddit" | "youtube")
-    check_platform "$target"
-    ;;
-  "nsfw")
-    nsfw_platforms=("pornhub" "xnxx" "xvideos")
-    for platform_nsfw in "${nsfw_platforms[@]}"; do
-      check_nsfw_platform "$platform_nsfw"
-    done
-    ;;
-  *)
-    echo -e "${red}[-] Invalid service target. Use 'all', a specific platform name, or 'nsfw'.\e[0m"
-    exit 1
-    ;;
-esac
+if [ "$choice" == "social" ]; then
+  echo -e "[${cyan}~${clean}] Searching social media platforms for username: ${purple}$username${clean}"
+  platforms=("twitter" "instagram" "facebook" "github" "linkedin" "tiktok" "snapchat" "reddit" "youtube")
+  for platform in "${platforms[@]}"; do
+    check_platform "$platform"
+  done
+elif [ "$choice" == "nsfw" ]; then
+  echo -e "[${cyan}~${clean}] Searching NSFW platforms for username: ${purple}$username${clean}"
+  nsfw_platforms=("pornhub" "xnxx" "xvideos")
+  for platform_nsfw in "${nsfw_platforms[@]}"; do
+    check_nsfw_platform "$platform_nsfw"
+  done
+else
+  echo "Invalid choice: $choice. Use 'social' or 'nsfw'."
+  exit 1
+fi
